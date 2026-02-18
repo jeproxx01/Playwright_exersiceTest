419ccd44dce8bdd9d7588f9270025306ab2d7425
Jeffrey Cesar <cesar.jeffrey@hnu.edu.ph>
2026-02-19 01:56:52 +0800

    Validation of error Handling  success

--- tests/exercise_1.spec.ts
+++ tests/exercise_1.spec.ts
@@ -10,18 +10,32 @@ import {test, expect} from '@playwright/test'
             test.beforeEach(async({page}) =>{
 
                 await page.goto('https://www.saucedemo.com/');
-                await page.getByPlaceholder('Username').fill('standard_user');
-                await page.getByPlaceholder('Password').fill('secret_sauce');
-                await page.getByRole('button', {name: 'Login'}).click();
+                
             });
            
         test('Verify User Login', async ({page}) => {
 
-           await expect (page).toHaveURL(/\/inventory.html/);
-           await expect (page.getByText('Products')).toBeVisible();
+
+
+            await page.getByPlaceholder('Username').fill('standard_user');
+            await page.getByPlaceholder('Password').fill('secret_sauce');
+            await page.getByRole('button', {name: 'Login'}).click();
+
+            await expect (page).toHaveURL(/\/inventory.html/);
+            await expect (page.getByText('Products')).toBeVisible();
 
         });
-           
+        
+            test('Validation of Error Handling', async ({page}) => {
+
+
+            await page.getByPlaceholder('Username').fill('locked_out_user r');
+            await page.getByPlaceholder('Password').fill('wrong_pass');
+            await page.getByRole('button', {name: 'Login'}).click();
+
+            await expect(page.getByText('Epic sadface')).toBeVisible();
+
+            });
        
 
 

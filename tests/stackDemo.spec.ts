0e678594ecf7900d28f8d3f17ad5542115f05927
Jeffrey Cesar <cesar.jeffrey@hnu.edu.ph>
2026-03-28 11:46:15 +0800

    Resolved stackDemo file

--- tests/stackDemo.spec.ts
+++ tests/stackDemo.spec.ts
@@ -1,13 +1,14 @@
 import {test, expect} from '@playwright/test'
 
 
-
+ 
 test('Verify Product Sorting', async({page}) => {
 
+    //1. 
     await page.goto('https://www.bstackdemo.com/');
  
 
-
+    //2. 
     const orderDropdown = page.locator('.sort select');
     
     
@@ -15,20 +16,53 @@ test('Verify Product Sorting', async({page}) => {
     await expect(orderDropdown).toBeEnabled();
 
     await orderDropdown.selectOption('lowestprice');
-   
+   await page.waitForTimeout(3000);
+ 
+    const name = page.locator('.shelf-item__title');
+    const prices = page.locator('.val b');
 
-    
-    
-});
-
-test('Retrive product name and price', async ({page}) => {
-
-    await page.goto('https://www.bstackdemo.com/');
-
-    const productNames = await page.locator('.shelf-item').allTextContents();
+    const productNames = await page.locator('.shelf-item__title').allTextContents();
     const productPrice = await page.locator('.val b').allTextContents();
 
 
-    console.log("product Price elements: ", productNames);
+    console.log("product names elements: ", productNames);
+    console.log("product prices", productPrice);
+
+    
+
+    //Verifies if both counts are equal
+    const prodCount = await name.count();
+    const priceCount = await prices.count();
+
+    if(prodCount === priceCount)
+
+    {
+        console.log('Prices counts are equal...');
+    }
+
+
+    //Prints Product Name along with its Price
+    console.log('product name length: ', productNames.length);
+    console.log('product price length: ', productPrice.length);
+    
+    expect(productNames && productPrice).toHaveLength(25);
+
+    for(let i=0; i<productNames.length; i++)
+    {
+        console.log(`Product Name: ${productNames[i]} | $${productPrice[i]}`);
+    }
+
+    const lowestName = productNames[0];
+    const lowestPrice = productPrice[0];
+
+    console.log('\n Lowest Price Product...');
+    console.log(`Lowest Name: ${lowestName} | lowest Price: $${lowestPrice}`);
+
+
+    const highestProd = productNames[24];
+    const highestPrice = productPrice[24];
+
+    console.log('\n Highest Product Name with Price...');
+    console.log(`Highest Name: ${highestProd} | Highest Price: $${highestPrice}`);
     
 });
